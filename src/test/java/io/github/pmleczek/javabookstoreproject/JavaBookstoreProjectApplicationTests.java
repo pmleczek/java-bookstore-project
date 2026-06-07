dfc5257a02950ea321c9d6a347c1fd94784f2d47
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2026-06-07 15:45:29 +0200

    build: initialize spring boot project with intellij idea

---
+++ src/test/java/io/github/pmleczek/javabookstoreproject/JavaBookstoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pmleczek.javabookstoreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBookstoreProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
